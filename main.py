8230cf9b13d274eb5a66152aa6d218970d98f791
vlazzari <vlazzari@uade.edu.ar>
2026-08-25 20:45:51 -0300

    mejoras

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ import estudiantes_materias
 usuario = "Admin"
 contrasenia = "123"
 
+RESET = "\033[0m"
+BOLD  = "\033[1m"
+ROJO  = "\033[31;1m"
+
 
 def validar_rango(desde, hasta):
     numero = int(input("Ingrese una opción: " ))
     while numero < desde or numero > hasta:
-        print("Opción inválida. Ingrese una opcion que se encuentre en el rango")
+        print(f"{ROJO}Opción inválida. Ingrese una opcion que se encuentre en el rango{RESET}")
         numero = int(input("Ingrese de nuevo una opción: "))
     return numero
 
@@ -21,8 +25,8 @@ def login(usuario_almacenado, contraseña_almacenada):
 
 #------------------------ Submenu ---------------------
 def mostrar_submenu(titulo):
-    print("""
-    === MENÚ""",titulo,"""===
+    print(f"""
+    === MENÚ {titulo} ===
     1. ALTA
     2. BAJA
     3. MODIFICACIÓN
@@ -31,40 +35,40 @@ def mostrar_submenu(titulo):
     """)
 
 #------------------ Menu Estudiantes ---------------------
-def menu_estudiantes(notas, estudiantes, materias):
+def menu_estudiantes(estudiantes, notas):
     mostrar_submenu("ESTUDIANTES")
     opcion = validar_rango(1, 5)
     match opcion:
         case 1:
             estudiantes_materias.altas_estudiantes_materias(estudiantes, "ESTUDIANTES")
-            menu_calificaciones(notas, estudiantes, materias)
+            menu_estudiantes(estudiantes, notas)
         case 2:
-            estudiantes_materias.bajas_estudiantes_materias(estudiantes, "ESTUDIANTES")
-            menu_calificaciones(notas, estudiantes, materias)
+            estudiantes_materias.bajas_estudiantes_materias(estudiantes, notas, "ESTUDIANTES")
+            menu_estudiantes(estudiantes, notas)
         case 3:
             estudiantes_materias.modificar_estudiantes_materias(estudiantes, "ESTUDIANTES")
-            menu_calificaciones(notas, estudiantes, materias)
+            menu_estudiantes(estudiantes, notas)
         case 4:
             estudiantes_materias.mostrar_estudiantes_materias(estudiantes, "ESTUDIANTES")
-            menu_calificaciones(notas, estudiantes, materias)
+            menu_estudiantes(estudiantes, notas)
 
 #------------------ Menu Materias ---------------------
-def menu_materias(notas, estudiantes, materias):
+def menu_materias(materias, notas):
     mostrar_submenu("MATERIAS")
     opcion = validar_rango(1, 5)
     match opcion:
         case 1:
             estudiantes_materias.altas_estudiantes_materias(materias, "MATERIAS")
-            menu_calificaciones(notas, estudiantes, materias)
+            menu_materias(materias, notas)
         case 2:
-            estudiantes_materias.bajas_estudiantes_materias(materias, "MATERIAS")
-            menu_calificaciones(notas, estudiantes, materias)
+            estudiantes_materias.bajas_estudiantes_materias(materias, notas, "MATERIAS")
+            menu_materias(materias, notas)
         case 3:
             estudiantes_materias.modificar_estudiantes_materias(materias, "MATERIAS")
-            menu_calificaciones(notas, estudiantes, materias)
+            menu_materias(materias, notas)
         case 4:
             estudiantes_materias.mostrar_estudiantes_materias(materias, "MATERIAS")
-            menu_calificaciones(notas, estudiantes, materias)
+            menu_materias(materias, notas)
 
 #------------------ Menu Calificaciones ---------------------
 def menu_calificaciones(notas, estudiantes, materias):
@@ -100,11 +104,14 @@ def mostrar_menu_principal():
 def ejecutar_menu_principal(eleccion, estudiantes, materias, notas):
     match eleccion:
         case 1:
-            menu_estudiantes(notas, estudiantes, materias)
+            menu_estudiantes(estudiantes, notas)
+            menu_principal(estudiantes, materias, notas)
         case 2:
-            menu_materias(notas, estudiantes, materias)
+            menu_materias(materias, notas)
+            menu_principal(estudiantes, materias, notas)
         case 3:
             menu_calificaciones(notas, estudiantes, materias)
+            menu_principal(estudiantes, materias, notas)
         case 4:
             ...
         case 5:
@@ -150,7 +157,7 @@ def principal():
     if login(usuario, contrasenia) == True:
         menu_principal(estudiantes, materias, notas)
     else:
-        print("Usuario o contraseña inválidos.")
+        print(f"{ROJO}Usuario o contraseña inválidos.{RESET}")
 
 
 principal()
